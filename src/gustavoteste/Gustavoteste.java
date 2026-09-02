b55277d58968e110dec01fa4d245f410eb728d9c
Gustavo <programadorinvestidor123@gmail.com>
2026-09-02 20:36:56 -0300

    Esqueci de colocar o -1 pra parar o programa ao digitar no console

--- src/gustavoteste/Gustavoteste.java
+++ src/gustavoteste/Gustavoteste.java
@@ -4,19 +4,44 @@
  */
 package gustavoteste;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Gusta
  */
 public class Gustavoteste {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello, world!");
-        // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+                
+       
+           // TODO code application logic here
+           
+       for (int i = 0; i <= 5; i++) {
+            System.out.println("nome");
+            String name = input.next();
+            
+            if( name == "-1") {
+                break;
+            }
+        
+            System.out.println("Nota: 1");
+            int n1 = input.nextInt();
+
+            System.out.print("nota 2");
+            int n2 = input.nextInt();
+
+            int media = (n1 * 2 + n2 * 3) / 5;
+
+
+            System.out.println(media);
+       }
     }
-    
 }
